89ec84244a2de73a24b0a5c047da661fd684a205
User <user@Users-MacBook-Pro.local>
2026-09-10 08:27:37 +0530

    commit

--- Demo.java
+++ Demo.java
@@ -7,6 +7,20 @@ public class Demo {
         short s = 10;
         int i = 20;
         long l =1000000;
+
+        //Real numbers
+        float f =23.45f;
+        double d = 34.5632;
+
+        //Characters
+        char c = 'a';
+
+        //boolean
+        boolean bo = true;
+
         System.out.println("Integer values -->" + b + "," + s + "," + i +"," +l);
+        System.out.println("Floating values -->" + f + "," + d );
+        System.out.println("Character values -->" + c);
+        System.out.println("boolean values -->" + bo);
     }
 }
