0b34031dd4a6f8fa8939364eb3b9b73e4bff89fe
User <user@Users-MacBook-Pro.local>
2026-09-10 08:19:19 +0530

    commit

---
+++ Demo.java
@@ -0,0 +1,12 @@
+
+public class Demo {
+
+    public static void main(String[] args){
+        //integers
+        byte b = 5;
+        short s = 10;
+        int i = 20;
+        long l =1000000;
+        System.out.println("Integer values -->" + b + "," + s + "," + i +"," +l);
+    }
+}
