da5486e49c189ed10c0884a902597a160bcd0b71
Annu9111 <aksoni911190@gmail.com>
2026-09-12 08:37:02 +0530

    commit

---
+++ lecture_4.java
@@ -0,0 +1,21 @@
+public class lecture_4 {
+    public static void main(String[] args){
+        //Operators
+        //Arithmatic operators --> +,-,/,%,+=,-=,%=,/=,++,-- etc.
+        int a=3;
+        int b=10;
+        int c=a+b;
+        int d=a-b;
+        int e=a*b;
+        int f=a/b;
+        int g=a%b;
+        System.out.println(c + "," + d +","+e+","+f+","+g);
+
+        int h = a+2;
+        h=h+2;
+        h+=2;    //both are same 
+        h-=2;
+        h*=3;
+        System.out.println(h);
+    }
+}
