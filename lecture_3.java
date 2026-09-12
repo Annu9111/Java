0dda2f1f7e20d12e398bb323b05db931bc318545
Annu9111 <aksoni911190@gmail.com>
2026-09-12 07:13:38 +0530

    commit

---
+++ lecture_3.java
@@ -0,0 +1,17 @@
+public class lecture_3 {
+    public static void main (String[] args){
+        //implicit conversion
+        //byte to int
+        // byte b= 24;
+        // int i;
+        // i=b; 
+        // System.out.println(i);
+
+        //character to int 
+        char c ='a';
+        int i; 
+        i=c;
+        System.out.println(i);
+         
+    }
+}
