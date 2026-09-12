b9fd3d3c03a274bc40ba94fbc663de43a3c9e3d4
Annu9111 <aksoni911190@gmail.com>
2026-09-12 09:13:14 +0530

    commit

---
+++ lecture_5.java
@@ -0,0 +1,18 @@
+public class lecture_5 {
+    public static void main(String[] args) {
+        //relational operators-->==,!=,<,>,>=,<=
+        int a=5;
+        int b=10;
+        boolean c = (a==b);
+        System.out.println(c);
+
+        boolean d =(a!=b);
+        System.out.println(d);
+
+        boolean e =(a<b);
+        System.out.println(e);
+
+        boolean f= (a>=b);
+        System.out.println(f);
+    }
+}
