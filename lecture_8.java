8c12a9e7d9f53dc27a6d0bdf713b63a28cdacad8
Annu9111 <aksoni911190@gmail.com>
2026-09-13 09:12:33 +0530

    commit

--- lecture_8.java
+++ lecture_8.java
@@ -1,17 +1,26 @@
 public class lecture_8 {
     public static void main(String[] args) {
         //iteration statements 
+
+        //while loop
         int i =1;
         while(i<=10){
             System.out.println(i);
             i++;
         }
 
+        //do while loop
         int j =0;
         do{
             System.out.println("welcome");
             System.out.println(j);
             j++;
         }while(j<=10);
+
+        //for loop
+        int k;
+        for(k=1;k<=10;k++){
+            System.out.println(k);
+        }
     }
 }
