90eee253de35c4c3645b4ca7c4693ce5bc7be946
Annu9111 <aksoni911190@gmail.com>
2026-09-15 09:55:40 +0530

    functions

--- lecture_10.java
+++ lecture_10.java
@@ -2,6 +2,25 @@ public class lecture_10 {
     public static void main(String[] args) {
         //⭐️functions
 
+        greet();
+
+        sayHello("Annu");
+
+        System.out.println(getOutput());
+
+        System.out.println(multiply(4, 9));
+
+
+        //⭐️Function overloading
+        int result1 = sum(5, 9);
+        System.out.println(result1);
+
+        int result2 = sum2(5,7,9);
+        System.out.println(result2);
+
+
+
+
     }
     //🔥no input no output
     static void greet(){
@@ -22,5 +41,12 @@ public class lecture_10 {
     //🔥 input as well as output
     static int multiply(int a,int b){
         return a*b;
+    }
+
+    static int sum(int p, int q){
+        return p+q;
+    }
+    static int sum2(int p, int q, int r){
+        return p+q+r;
     }
 }
