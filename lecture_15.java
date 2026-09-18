eb0e1ea06e322bf3b25491b230d03a4ea9ac4688
Annu9111 <aksoni911190@gmail.com>
2026-09-18 08:57:38 +0530

    Static

---
+++ lecture_15.java
@@ -0,0 +1,25 @@
+//static keyword
+public class lecture_15 {
+    public static void main(String[] args) {
+        Student s1 = new Student("priya", 20, 9);
+        Student s2 = new Student("riya", 20, 15);
+
+        Student.collegeName = "IIT bombay";
+        System.out.println(s1.name +", "+s1.age +" ,"+s1.rollNo+" ,"+Student.collegeName);
+
+        System.out.println(s2.name +", "+s2.age +" ,"+s2.rollNo+" ,"+Student.collegeName);
+    }
+}
+
+class Student{
+    String name;
+    int age;
+    int rollNo;
+    static String collegeName;
+
+    Student(String name, int age , int rollNo){
+        this.name = name;
+        this.age = age;
+        this.rollNo = rollNo;
+    }
+}
