cc9a9fa48e43acb6d2ad8a5c206e101cafb589e5
Annu9111 <aksoni911190@gmail.com>
2026-09-22 07:18:49 +0530

    interface

--- lecture_21.java
+++ lecture_21.java
@@ -5,5 +5,32 @@ public class lecture_21 {
 }
 
 interface Car{
-    
+    void Start();
+    void Accelerate();
+    void Brake();
 }
+class Fcar implements Car{
+    public void Accelerate(){
+        System.out.println("fuel car is accelerated");
+    }
+    public void Brake(){
+        System.out.println("Fuel car is stopped");
+    }
+    public void Start(){
+        System.out.println("FCar is Started");
+    }
+
+}
+class Ecar implements Car{
+    public void Accelerate(){
+        System.out.println("Ecar is accelerated !");
+    }
+    public void Brake(){
+        System.out.println("Ecar is Breaked");
+    }
+    public void Start(){
+        System.out.println("ECar is started");
+    }
+}
+
+ 
