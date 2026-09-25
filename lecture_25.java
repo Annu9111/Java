4a30b69a97c932cc5109020d548b1823c3d50e95
Annu9111 <aksoni911190@gmail.com>
2026-09-25 09:57:11 +0530

    system.in.read()

---
+++ lecture_25.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+public class lecture_25 {
+    public static void main(String[] args) throws IOException {
+        //console i/o
+        System.out.println("Enter number");
+        int x = System.in.read();
+        System.out.println(x);
+
+    }
+}
